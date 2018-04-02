4d9b38dcad82e92f70f752e0ee38ffbc62e51768
Alex Gamper <agamper@bigpond.net.au>
2018-04-02 11:25:25 +1000

    Add copywrite notice to bindings

--- Bindings/Windows.ApplicationModel.Resources.Core.ads
+++ Bindings/Windows.ApplicationModel.Resources.Core.ads
@@ -1,6 +1,23 @@
 --------------------------------------------------------------------------------
---    Copywrite : 2018 - Alexander Gamper                                     --
+--                                                                            --
+--    Copyright(c) 2018 Alexander Gamper, All Rights Reserved.                --
+--                                                                            --
+--    Ada-WinRT                                                               --
 --    Version   : 0.1.0.0                                                     --
+--                                                                            --
+--    This program is free software: you can redistribute it and / or modify  --
+--    it under the terms of the GNU General Public License as published by    --
+--    the Free Software Foundation, either version 3 of the License, or       --
+--    (at your option) any later version.                                     --
+--                                                                            --
+--    This program is distributed in the hope that it will be useful,         --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of          --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           --
+--    GNU General Public License for more details.                            --
+--                                                                            --
+--    You should have received a copy of the GNU General Public License       --
+--    along with this program.If not, see < http://www.gnu.org/licenses/>.    --
+--                                                                            --
 --------------------------------------------------------------------------------
 with Windows.Foundation.Collections;
 limited with Windows.Storage;
