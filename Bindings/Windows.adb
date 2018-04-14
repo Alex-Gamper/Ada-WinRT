e9889774ee6f369204b7617d0e5354e93addd191
Alex Gamper <agamper@bigpond.net.au>
2018-04-14 12:48:24 +1000

    Relicense as LGPL v3

--- Bindings/Windows.adb
+++ Bindings/Windows.adb
@@ -3,7 +3,7 @@
 --    Copyright(c) 2018 Alexander Gamper, All Rights Reserved.                --
 --                                                                            --
 --    Ada-WinRT                                                               --
---    Version   : 0.1.0.                                                      --
+--    Version   : 0.1.0.1                                                     --
 --                                                                            --
 -- This program is free software: you can redistribute it and / or modify     --
 -- it under the terms of the GNU Lesser General Public License as published by--
